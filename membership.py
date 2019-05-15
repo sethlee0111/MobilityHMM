1dfd91073f9b70f8c7199ad93057004920147b98
WJerry0227 <wangjiawei0227@163.com>
2019-05-15 15:17:43 -0500

    Add: new method for membership

--- membership.py
+++ membership.py
@@ -2,28 +2,39 @@ import pandas as pd
 import numpy as np
 
 class MembershipVector():
-    """
-    userList: a list of user ID
-    groupNum: a integer
-    """
     def __init__(self, userList, groupNum):
-        # randomly initilize the prob
-        # init the userID and group Probability to a map of user
+        """
+        randomly initilize the prob
+        init the userID and group Probability to a map of user'
+
+        Args:
+            userList: a list of user ID
+            groupNum: a integer
+        """
         self.dict = {}
         self.userList = userList
         for i in userList:
             self.dict[i] = np.random.dirichlet(np.ones(groupNum))
-        print(self.dict)
-        
-    """ return group prob list of the input user
-    """
-    def getProbByUserId(self, userId):
 
+    def getUserProbByGroup(self, userId, groupId):
+        """
+        Get the probability that the user belongs to this group
+        """
+        return
+
+    def getProbByUserId(self, userId):
+        """
+        Args:
+            return group prob list of the input user
+        """
         return self.dict[userId].tolist()
 
-    """ return userID in the input group, group ID starts from 0
-    """
+
     def getUserByGroup(self, groupId):
+        """
+        Args:
+            return userID in the input group, group ID starts from 0
+        """
         groupList=[]
         for i in self.userList:
             userProb = self.dict[i].tolist()

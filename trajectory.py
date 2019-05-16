6c041e55b538c1252b5ae55c17aa143f49b80613
WJerry0227 <wangjiawei0227@163.com>
2019-05-15 22:13:59 -0500

    Update: convert category to int

--- trajectory.py
+++ trajectory.py
@@ -5,21 +5,26 @@ from membership import MembershipVector
 class Trajectory():
     def __init__(self, group, dataframe):
         self._df = dataframe
-        self._trajectoryData = self.generateTrajectoryData()
+
+        data = self._df.drop(columns='Timegap')
+        data = data[['UserID','Latitude','Longitude','Time','Venue category name','Trajectory']]
+        data['Time'] = data['Time'].apply(lambda x: str(x)[-5:])
+        # encode venue category to id
+        data['Venue category name'] = pd.Categorical(data['Venue category name'])
+        data['VenueID'] = data['Venue category name'].cat.codes
+        self._venuedDict = dict(enumerate(data['Venue category name'].cat.categories))
+        
+        # print(data['Time'].apply(lambda x: (int(str(x)[0:2]) * 24 + int(str(x)[3:5]))))
+        self._data = data
         # initilize the membership vector
         self._member = MembershipVector(trajectorydata['UserID'].unique(), group)
         self._group = group
 
-    def generateTrajectoryData(self):
-        data = self._df.drop(columns='Timegap')
-        data = data[['UserID','Latitude','Longitude','Time','Venue category name','Trajectory']]
-        data['Time'] = data['Time'].apply(lambda x: str(x)[-5:])
-        return data
 
     def getData(self, groupId):
         """get training data based on group
         """
-        rawdata = self.generateTrajectoryData()
+        rawdata = self._data
         length = np.asarray(rawdata.groupby('Trajectory').count()['Time'])
         ## Get probability of the user belongs to group
         proba = []
@@ -27,11 +32,13 @@ class Trajectory():
         for user in userList:
             proba.append(self._member.getUserProbByGroup(userId=user[0],groupId=groupId))
         data = rawdata.drop(columns='Trajectory')
+        data = data.drop(columns='Venue category name')
         data = data.drop(columns='UserID')
+        print(data)
         return (data.values, length, proba)
 
     def getTrajectoryByUser(self, userId):
-        data = self._trajectoryData.loc[self._trajectoryData['UserID'] == userId]
+        data = self._data.loc[self._data['UserID'] == userId]
         data = data.drop(columns='UserID')
         return data
 

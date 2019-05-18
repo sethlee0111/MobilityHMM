3357d85f51b7b31925edb41c661f74c61a3d3f9e
WJerry0227 <wangjiawei0227@163.com>
2019-05-18 16:01:25 -0500

    group update

--- group.py
+++ group.py
@@ -5,21 +5,22 @@ from membership import MembershipVector
 from trajectory import Trajectory
 
 class Group():
-    def __init__(self, hmm, membership, trajectory):
+    def __init__(self, hmm, membership, trajectory, groupId):
         self._hmm = hmm
         self._membership = membership
         self._trajectory = trajectory
+        self._groupId = groupId
     
-    def update(self, groupId):
+    def update(self):
         member = self._membership
         # X is a trajectory for each user
-        p_g = self._membership.getMeanProbByGroup(groupId)
+        p_g = self._membership.getMeanProbByGroup(self._groupId)
         for userId in member.userList:
             trajectoryArray = self._trajectory.getTrajectoryByUser(userId)
             p_ugH = 1
             for trajectory in trajectoryArray:
                 p_ugh = p_ugh * self._hmm._compute_log_likelihood(trajectory)
             p_guH = p_ugH * p_g
-            member.setProbByGroupUser(p_guH, userId, groupId)
+            member.setProbByGroupUser(p_guH, userId, self._groupId)
 
         return member
